f34f8dee36d4c8cf12e26bb15556e8f0e4d9e072
Alaan07 <rakeshofficial007@gmail.com>
2025-07-31 14:21:46 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import AllBlogs from "./Components/AllBlogs.jsx";
 import ContactPage from "./Components/ContactPage";
 import Navbar from "./Components/Navbar";
@@ -19,36 +19,44 @@ import EditAchievement from "./Components/EditAchievement";
 import EditProfile from "./Components/EditProfile";
 import Eventpage from "./Components/Eventpage";
 import Footer from './Components/Footer';
-import About from './Components/About';
 
+import { BrowserRouter as Router } from "react-router-dom";
+
+function Layout() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === "/dashbord";
+
+  return (
+    <>
+      {!hideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashbord" element={<Dashboard />} />
+        <Route path="/allblogs" element={<AllBlogs />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/achievements" element={<Achievement />} />
+        <Route path="/events" element={<Event />} />
+        <Route path="/addblog" element={<AddBlog />} />
+        <Route path="/addevent" element={<AddEvents />} />
+        <Route path="/addachievement" element={<AddAchievement />} />
+        <Route path="/editblogs/:id" element={<EditBlogs />} />
+        <Route path="/editevents" element={<EditEvent />} />
+        <Route path="/editachievement" element={<EditAchievement />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/eventpage" element={<Eventpage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/dashbord" element={<Dashboard />} />
-          <Route path="/allblogs" element={<AllBlogs />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/achievements" element={<Achievement />} />
-          <Route path="/events" element={<Event />} />
-          <Route path="/addblog" element={<AddBlog />} />
-          <Route path="/addevent" element={<AddEvents />} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/addachievement" element={<AddAchievement />} />
-          <Route path="/editblogs/:id" element={<EditBlogs />} />
-          <Route path="/editevents" element={<EditEvent />} />
-          <Route path="/editachievement" element={<EditAchievement />} />
-          <Route path="/editprofile" element={<EditProfile />} />
-          <Route path="/eventpage" element={<Eventpage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <Router>
+      <Layout />
+    </Router>
   );
 }
 
