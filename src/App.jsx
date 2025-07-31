e0c5341a08e5d27a1edb936a8e9883d6dad9894d
Soundarya R <soundaryaramachandra2003@email.com>
2025-07-31 15:33:27 +0530

    Update support form styles with max-height constraint

--- src/App.jsx
+++ src/App.jsx
@@ -20,32 +20,55 @@ import EditProfile from "./Components/EditProfile";
 import Eventpage from "./Components/Eventpage";
 import Footer from './Components/Footer';
 
-function App() {
+import { BrowserRouter as Router } from "react-router-dom";
+
+function Layout() {
+  const hideNavbarPaths = [
+    "/dashbord",
+    "/blogs",
+    "/achievements",
+    "/events",
+    "/addblog",
+    "/addevent",
+    "/addachievement",
+    "/editblogs/:id",
+    "/editevents",
+    "/editachievement",
+    "/editprofile"
+
+  ];
+  const hideNavbar = hideNavbarPaths.includes(location.pathname);  
   return (
     <>
-      <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/dashbord" element={<Dashboard />} />
-          <Route path="/allblogs" element={<AllBlogs />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/achievements" element={<Achievement />} />
-          <Route path="/events" element={<Event />} />
-          <Route path="/addblog" element={<AddBlog />} />
-          <Route path="/addevent" element={<AddEvents />} />
-          <Route path="/addachievement" element={<AddAchievement />} />
-          <Route path="/editblogs/:id" element={<EditBlogs />} />
-          <Route path="/editevents" element={<EditEvent />} />
-          <Route path="/editachievement" element={<EditAchievement />} />
-          <Route path="/editprofile" element={<EditProfile />} />
-          <Route path="/eventpage" element={<Eventpage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {!hideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashbord" element={<Dashboard />} />
+        <Route path="/allblogs" element={<AllBlogs />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/achievements" element={<Achievement />} />
+        <Route path="/events" element={<Event />} />
+        <Route path="/addblog" element={<AddBlog />} />
+        <Route path="/addevent" element={<AddEvents />} />
+        <Route path="/addachievement" element={<AddAchievement />} />
+        <Route path="/editblogs/:id" element={<EditBlogs />} />
+        <Route path="/editevents" element={<EditEvent />} />
+        <Route path="/editachievement" element={<EditAchievement />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/eventpage" element={<Eventpage />} />
+      </Routes>
+      <Footer />
     </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
   );
 }
 
